de4cb36db69e3648c701a64ac0c3584dcfa8b026
Ronan26 <ronansingh46@gmail.com>
2023-11-28 20:36:57 +0545

    com

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { Route, Routes } from 'react-router'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Notfound from './components/Notfound'
 
-export default App;
+import About from './pages/About'
+import RootLayOut from './components/RootLayOut'
+import HomepAge from './components/HomepAge'
+import DetailPage from './pages/DetailPage'
+
+
+
+
+const App = () => {
+return (
+<>
+ <Routes>
+<Route path='/' element={<RootLayOut />}>
+<Route index element={<HomepAge />} />
+<Route path='detail/:id' element={<DetailPage />} />
+<Route path='about-page' element={<About />} />
+<Route path='*' element={<Notfound />} />
+</Route>
+
+    </Routes>
+     </>
+    
+  )
+  
+
+  }
+export default App
+
